a235960f5906f55836ca04bf944e44965c3203a5
Tanishq Bhatia <tanishqbhatia474@gmail.com>
2026-01-07 21:27:49 +0530

    feat: add GET /api/family endpoint to fetch invite code

--- backend/src/controllers/family.controller.js
+++ backend/src/controllers/family.controller.js
@@ -1,4 +1,4 @@
-import { createFamilyService } from '../services/family.service.js';
+import { createFamilyService, getFamilyDetailsService } from '../services/family.service.js';
 import { joinFamilyService } from '../services/family.service.js';
 export const createFamily = async (req, res, next) => {
   try {
@@ -13,6 +13,15 @@ export const joinFamily = async (req, res, next) => {
   try {
     const result = await joinFamilyService(req.user, req.body);
     res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getFamilyDetails = async (req, res, next) => {
+  try {
+    const family = await getFamilyDetailsService(req.user);
+    res.status(200).json(family);
   } catch (err) {
     next(err);
   }
